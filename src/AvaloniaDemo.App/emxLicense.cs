793d626b431bcf28ae4875417c4153cdc6368aba
Михаил <mike_shubin@mail.ru>
2026-01-26 17:08:07 +0300

    Add EMX Controls license and configure invariant globalization

--- src/AvaloniaDemo.App/emxLicense.cs
+++ src/AvaloniaDemo.App/emxLicense.cs
@@ -1,20 +1,16 @@
-namespace AvaloniaDemo.App;
+// This file is auto-generated. Please do not change it.
+using System.Runtime.CompilerServices;
 
-/// <summary>
-/// EMX Controls License configuration.
-/// </summary>
-/// <remarks>
-/// To use EMX Controls without trial limitations, you need to obtain a license key
-/// from https://eremexcontrols.net and set it here.
-/// </remarks>
-public static class EmxLicense
+using Eremex.AvaloniaUI.Controls.License;
+
+namespace AvaloniaDemo.App;
+public class LicenseProvider
 {
-    /// <summary>
-    /// Initializes the EMX Controls license.
-    /// </summary>
-    public static void Register()
-    {
-        // Uncomment and set your license key below:
-        // Eremex.AvaloniaUI.Controls.Licensing.EmxLicense.SetLicense("YOUR_LICENSE_KEY_HERE");
-    }
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+	[ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    public static void RegisterLicense()
+	{
+        ControlsLicenseManager.SetRuntimeLicenseOwner(new LicenseProvider(),"msin", "000003E7", "C4 F8 C6 43 42 E1 AD 33 7E 74 3A A2 DB 60 91 2F C8 9C BB A0 08 92 76 52 B9 86 C9 9E 10 3B 94 BF 1C 17 40 F4 8D 99 11 43", "93 8F 75 1D B0 D6 E9 98 6C 8E 8A F8 22 25 6A E4 35 41 78 FC D0 31 CC 03 39 9A 4E F4 7D F6 7A 6C 7F EC 25 D7 AC 68 BA 31");
+	}
 }
