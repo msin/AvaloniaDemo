40c24809838093124f7f8a36f65cdff5f11c6c1d
Михаил <mike_shubin@mail.ru>
2026-01-26 16:44:46 +0300

    Add DataGridControl with manually defined columns and Light theme

--- src/AvaloniaDemo.App/ViewModels/MainWindowViewModel.cs
+++ src/AvaloniaDemo.App/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,41 @@
-﻿namespace AvaloniaDemo.App.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+
+namespace AvaloniaDemo.App.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private IList<EmployeeInfo> employees;
+
+    public MainWindowViewModel()
+    {
+        Employees = new List<EmployeeInfo>
+        {
+            new EmployeeInfo("Alex", "Smith", new DateTime(1990, 1, 1)),
+            new EmployeeInfo("Samantha", "Brown", new DateTime(1988, 2, 5)),
+            new EmployeeInfo("Nick", "Morris", new DateTime(2000, 8, 25)),
+            new EmployeeInfo("Julia", "Lee", new DateTime(2005, 12, 3))
+        };
+    }
+}
+
+public partial class EmployeeInfo : ObservableObject
+{
+    [ObservableProperty]
+    private string firstName;
+
+    [ObservableProperty]
+    private string lastName;
+
+    [ObservableProperty]
+    private DateTime birthDate;
+
+    public EmployeeInfo(string firstName, string lastName, DateTime birthDate)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        BirthDate = birthDate;
+    }
 }
