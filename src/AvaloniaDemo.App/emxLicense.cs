08f926ce54422797db9c1dd457cf7f92a9d82939
Михаил <mike_shubin@mail.ru>
2026-01-26 16:59:50 +0300

    Add EMX Controls license configuration

---
+++ src/AvaloniaDemo.App/emxLicense.cs
@@ -0,0 +1,20 @@
+namespace AvaloniaDemo.App;
+
+/// <summary>
+/// EMX Controls License configuration.
+/// </summary>
+/// <remarks>
+/// To use EMX Controls without trial limitations, you need to obtain a license key
+/// from https://eremexcontrols.net and set it here.
+/// </remarks>
+public static class EmxLicense
+{
+    /// <summary>
+    /// Initializes the EMX Controls license.
+    /// </summary>
+    public static void Register()
+    {
+        // Uncomment and set your license key below:
+        // Eremex.AvaloniaUI.Controls.Licensing.EmxLicense.SetLicense("YOUR_LICENSE_KEY_HERE");
+    }
+}
